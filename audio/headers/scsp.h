66d69b9378dbed31b01afa9a35feaecbbaf0aacd
Jacob Praskala <praskalaj@gmail.com>
2024-08-02 00:44:57 -0400

    Added new registers

--- audio/headers/scsp.h
+++ audio/headers/scsp.h
@@ -1,10 +1,63 @@
 #pragma once
 #include <stdint.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef  uint32_t u32;
+typedef uint32_t u32;
+typedef uint16_t u16;
 typedef uint8_t u8;
 
+// Individual Slot Control Registers
+#define KYONEX *(volatile u32*)0x00
+#define KYONB *(volatile u32*)0x00
+#define SBCTL *(volatile u32*)0x00
+#define SSCTL *(volatile u32*)0x00
+#define LPCTL *(volatile u32*)0x00
+#define PCM8B *(volatile u32*)0x00
+#define SA *(volatile u32*)0x00
+
+#define SA2 *(volatile u32*)0x02
+
+#define LSA *(volatile u32*)0x04
+
+#define LEA *(volatile u32*)0x06
+
+#define D2R *(volatile u32*)0x08
+#define D1R *(volatile u32*)0x08
+#define EGHOLD *(volatile u32*)0x08
+#define AR *(volatile u32*)0x08
+
+#define LPSLNK *(volatile u32*)0x0A
+#define KRS *(volatile u32*)0x0A
+#define DL *(volatile u32*)0x0A
+#define RR *(volatile u32*)0x0A
+
+#define STWINH *(volatile u32*)0x0C
+#define SDIR *(volatile u32*)0x0C
+#define TL *(volatile u32*)0x0C
+
+#define MDL *(volatile u32*)0x0E
+#define MDXSL *(volatile u32*)0x0E
+#define MDYSL *(volatile u32*)0x0E
+
+#define OCT *(volatile u32*)0x10
+#define FNS *(volatile u32*)0x10
+
+#define LFORE *(volatile u32*)0x12
+#define LFOF *(volatile u32*)0x12
+#define PLFOWS *(volatile u32*)0x12
+#define PLFOS *(volatile u32*)0x12
+#define ALFOWS *(volatile u32*)0x12
+#define ALFOS *(volatile u32*)0x12
+
+#define ISEL *(volatile u32*)0x14
+#define OMXL *(volatile u32*)0x14
+
+#define DISDL *(volatile u32*)0x16
+#define DIPAN *(volatile u32*)0x16
+#define EFSDL *(volatile u32*)0x16
+#define EFPAN *(volatile u32*)0x16
 
 // Control Registers
 
@@ -65,6 +118,4 @@ typedef uint8_t u8;
 
 #define MCIRE *(volatile u32*)0x25B0042E
 
-
-// Timer Registers
 
